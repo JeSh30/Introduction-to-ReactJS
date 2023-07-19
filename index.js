7d38dcb5f6a2fcced05f755c9c8859e3baa3cb5b
JeSh30 <rajeshanumala1230@gmail.com>
2023-07-19 10:44:35 +0000

    solution

---
+++ index.js
@@ -0,0 +1,7 @@
+const elementContainer = (
+  <div>
+    <h1> Hello User </h1>
+    <p> Have a Nice to meet You </p>
+  </div>
+);
+ReactDOM.render(elementContainer, document.getElementById("container"));
